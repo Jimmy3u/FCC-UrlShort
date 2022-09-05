33c3055b471f24c43b30dbecd721080d06ebe425
Jimmy3u <jpp.olv@gmail.com>
2022-09-05 16:59:05 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,62 +1,64 @@
+//Imports
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const dns = require('dns');
+const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose();
+
+//Express Initialization
 app.use(bodyParser.urlencoded({ extended: "false" }));
 app.use(bodyParser.json());
-
-
-//Functions
-
-
+app.listen(3000, () => console.log("Server listening on port 3000"));
+app.use(cors());
 
 //Database things
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database(':memory:', (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Connected to the in-memory SQlite database.');
+const insertStatement = 'INSERT INTO shortUrls(UrlLink) VALUES (?)'
+const selectStatment = 'SELECT * FROM shortUrls WHERE UrlId = ?'
 
+let db = new sqlite3.Database(':memory:', (err) => {
+    if (err) return console.error(err.message);
+    console.log('Connected to the in-memory SQlite database.');
+    dbStart();
+    });
+    
+const dbStart = () => {
     db.serialize(() => {
 
         db.run(`CREATE TABLE shortUrls (
         UrlId INTEGER PRIMARY KEY AUTOINCREMENT,
         UrlLink TEXT NOT NULL );`, (err) => (err) ? console.log(err) : console.log("Tabela criada com sucesso")),
 
-            db.run('INSERT INTO shortUrls(UrlLink) VALUES ("https://google.com")', function (err) {
-                if (err) return console.log(err.message);
-            })
-    }
-    )
-}
-);
+        db.run(insertStatement, ('https://google.com'), function (err) {
+            if (err) return console.log(err.message);
+        })
+    })
+};
+
 
 
 //Routing go here
-app.get('/', (req, res) => db.get('SELECT * FROM shortUrls WHERE UrlId = ?', [2], (err, row) => {
-    if (err) {
-        throw err;
-    }
-    return res.send(row);
-}))
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
 
 app.get('/api/shorturl/?:id', (req, res) => {
-    id = req.params.id
-    db.get(`SELECT * FROM shortUrls WHERE UrlId =${id}`, function (err, row) {
+    db.get(selectStatment, req.params.id, function (err, row) {
         if (err) res.send(err)
 
         if (row === undefined)
             res.send({ message: "URL NOT FOUND" })
         else
-            res.send({ shortId: row.UrlId })
+            res.redirect(row.UrlLink)
     })
 })
 
 app.post('/api/shorturl', (req, res) => {
 
-    function insertIntoDB(link) {
-        db.run(`INSERT INTO shortUrls(UrlLink) VALUES ("${link}")`, function () {
+    function insertThenReturnJSON(link) {
+        db.run(insertStatement, [link], function () {
             res.json({
                 original_url: link,
                 short_url: this.lastID
@@ -64,13 +66,14 @@ app.post('/api/shorturl', (req, res) => {
         })
     }
 
-    const url = new URL(req.body.u)
     const httpCheck = new RegExp("^(http|https)://*", "i")
-
-    if (httpCheck.test(url))
+    if (httpCheck.test(req.body.url)) {
+        const url = new URL(req.body.url)
         dns.lookup(url.hostname, { all: true }, (err, adress, family) => {
-            err ? res.json({ error: "Invalid Hostname" }) : insertIntoDB(url)
+            err ? res.json({ error: "Invalid Hostname" }) : insertThenReturnJSON(url)
         })
+    } else {
+        res.json({ error: "invalid url" })
+    }
+
 })
-//Listen on port x
-app.listen(3000, () => console.log("Server listening on port 3000"));
